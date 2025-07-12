26ba719f88cf1ee6f1aa86e416b15bcbc09c7c36
Pedro Henrique <pedrohfidg@gmail.com>
2025-07-12 16:32:19 -0300

    feat: Add findByTitle method to TaskRepository for title validation

--- src/main/java/com/example/taskwise/repository/TaskRepository.java
+++ src/main/java/com/example/taskwise/repository/TaskRepository.java
@@ -3,5 +3,9 @@ package com.example.taskwise.repository;
 import com.example.taskwise.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    Optional<Task> findByTitle(String title);
 }
