0954ded35125c22d45c3c185b805103f8ff06bc7
Pedro Henrique <pedrohfidg@gmail.com>
2025-07-12 15:21:16 -0300

    chore: Initial project setup with Spring Boot

---
+++ src/main/java/com/example/taskwise/TaskwiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskwiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taskwise/TaskwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
