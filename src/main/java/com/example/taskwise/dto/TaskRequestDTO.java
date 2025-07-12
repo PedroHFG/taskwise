203f65b4c37340241aba97401a106413f040d9ee
Pedro Henrique <pedrohfidg@gmail.com>
2025-07-12 16:22:37 -0300

    feat: Implement TaskRequestDTO and TaskResponseDTO

---
+++ src/main/java/com/example/taskwise/dto/TaskRequestDTO.java
@@ -0,0 +1,51 @@
+package com.example.taskwise.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public class TaskRequestDTO {
+
+    @NotBlank(message = "O título não pode estar vazio.")
+    private String title;
+
+    private String description;
+
+    @NotNull(message = "A data de vencimento não pode ser nula.")
+    private LocalDate dueDate;
+
+    public TaskRequestDTO() {
+
+    }
+
+    public TaskRequestDTO(String title, String description, LocalDate dueDate) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+}
---
+++ src/main/java/com/example/taskwise/dto/TaskResponseDTO.java
@@ -0,0 +1,98 @@
+package com.example.taskwise.dto;
+
+import com.example.taskwise.model.Task;
+import jakarta.persistence.Column;
+
+import java.time.Instant;
+import java.time.LocalDate;
+
+public class TaskResponseDTO {
+
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private Boolean completed;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    public TaskResponseDTO() {
+
+    }
+
+    public TaskResponseDTO(Long id, String title, String description, LocalDate dueDate, Boolean completed, Instant createdAt, Instant updatedAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.completed = completed;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public TaskResponseDTO(Task entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.description = entity.getDescription();
+        this.dueDate = entity.getDueDate();
+        this.completed = entity.getCompleted();
+        this.createdAt = entity.getCreatedAt();
+        this.updatedAt = entity.getUpdatedAt();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
